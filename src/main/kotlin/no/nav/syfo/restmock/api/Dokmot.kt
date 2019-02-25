fe295f777d8c44a365d0fad7e1c0b2ca1e17c65a
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-02-25 13:28:38 +0100

    Make sure the incoming request is expected and extract the ID for the sykmelding

--- src/main/kotlin/no/nav/syfo/restmock/api/Dokmot.kt
+++ src/main/kotlin/no/nav/syfo/restmock/api/Dokmot.kt
@@ -1,18 +1,29 @@
 package no.nav.syfo.restmock.api
 
 import io.ktor.application.call
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.post
+import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.syfo.restmock.StepType
 import no.nav.syfo.restmock.addStepFor
+import no.nav.syfo.restmock.model.MottaInngaaendeForsendelse
 import no.nav.syfo.restmock.model.MottaInngaandeForsendelseResultat
-import java.util.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.UUID
+
+val log: Logger = LoggerFactory.getLogger("dokmot-mock")
 
 fun Routing.registerDokmotApi() {
     post("/rest/mottaInngaaendeForsendelse") {
-        val smId = "TODO" // forsendelseInformasjon -> kanalReferanseId
+        val journalpost = call.receive<MottaInngaaendeForsendelse>()
+
+        val smId = journalpost.forsendelseInformasjon.kanalReferanseId
+        log.info("Received a request for persisting incoming document with {}", keyValue("smId", smId))
         addStepFor(smId, StepType.JOURNAL_INCOMING)
+
         call.respond(MottaInngaandeForsendelseResultat(
             UUID.randomUUID().toString(), "ENDELIG", listOf()
         ))
